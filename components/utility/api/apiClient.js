031bbb166acb49a680da61ea14b6bfe114022b47
jackjk521 <jedchu541@gmail.com>
2023-11-08 00:17:21 +0800

    Updated apiClient

--- components/utility/api/apiClient.js
+++ components/utility/api/apiClient.js
@@ -14,7 +14,8 @@ import axios from "axios";
 const apiClient = axios.create({
   baseURL: "https://color-city-backend.onrender.com/color_city/api",
   headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
+    "Content-Type": "application/x-www-form-urlencoded", 
+    "Access-Control-Allow-Origin": "https://color-city-backend.onrender.com"
     // "Cache-Control": "public, s-maxage=10, stale-while-revalidate=59",
     // Other headers if needed
   },
