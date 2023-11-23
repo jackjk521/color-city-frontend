2f286d87ea70605945c1e3a15b22f20d08fdeb83
jackjk521 <jedchu541@gmail.com>
2023-11-23 22:11:48 +0800

    Updated apiClient for production use

--- components/utility/api/apiClient.js
+++ components/utility/api/apiClient.js
@@ -1,24 +1,24 @@
 import axios from "axios";
 
-const apiClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/color_city/api",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    // "Cache-Control": "public, s-maxage=10, stale-while-revalidate=59",
-    // Other headers if needed
-  },
-  // Other configuration options if needed
-});
-
-// Production Only
 // const apiClient = axios.create({
-//   baseURL: "https://color-city-backend.onrender.com/color_city/api",
+//   baseURL: "http://127.0.0.1:8000/color_city/api",
 //   headers: {
-//     "Content-Type": "application/x-www-form-urlencoded", 
+//     "Content-Type": "application/x-www-form-urlencoded",
 //     // "Cache-Control": "public, s-maxage=10, stale-while-revalidate=59",
 //     // Other headers if needed
 //   },
 //   // Other configuration options if needed
 // });
+
+// Production Only
+const apiClient = axios.create({
+  baseURL: "https://color-city-backend.onrender.com/color_city/api",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded", 
+    // "Cache-Control": "public, s-maxage=10, stale-while-revalidate=59",
+    // Other headers if needed
+  },
+  // Other configuration options if needed
+});
 
 export default apiClient;
